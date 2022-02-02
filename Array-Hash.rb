864dbf5bf64d95dc8a68f62383a0f4b684136c47
Luis Felipe Tejada Padilla <lftejadap@uqvirtual.edu.co>
2022-02-01 19:10:10 -0500

    Practice case expressions and loops

--- Array-Hash.rb
+++ Array-Hash.rb
@@ -1,14 +1,16 @@
 # Array
 friends = Array["Alejandro","Juan","Nicolas"]
 friends[4] = "Andres"
-/
+
+=begin
 puts friends[2,3]
 puts "----"
 newFrieds = Array.new
 newFrieds[0] = "Luis"
 newFrieds[4] = "Julio"
 puts newFrieds
-/
+=end
+
 puts ("Size of array "+friends.length().to_s());
 puts ("Exist Juan "+ (friends.include? "Juan").to_s() )
 
--- caseExp-loops.rb
+++ caseExp-loops.rb
@@ -29,4 +29,33 @@ while index <= 5
     puts index
     index += 1 
 end
-    
+puts "----------"
+for idx in 5..9
+    puts idx
+end
+
+friends = Array["Alejandro","Juan","Nicolas","Pedro","Julian"]
+
+for friend in friends
+    puts friend
+end
+puts "----------"
+
+friends.each do |friend|
+    puts friend
+end
+puts "----------"
+
+6.times do |idx|
+    puts idx
+end
+
+def pow(base_num, pow_num)
+    result = 1
+    pow_num.times do #|mult|
+        result *= base_num 
+    end
+    return result
+end
+
+puts pow(2,4)
